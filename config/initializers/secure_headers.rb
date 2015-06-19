51019551691e3687387b5673f05a676789c41c91
Drew Bailey <drewbailey5@gmail.com>
2015-06-19 10:49:01 -0400

    adding secure headers

---
+++ config/initializers/secure_headers.rb
@@ -0,0 +1,8 @@
+::SecureHeaders::Configuration.configure do |config|
+  config.hsts = { :max_age => 20.years.to_i, :include_subdomains => true }
+  config.x_frame_options = 'DENY'
+  config.x_content_type_options = 'nosniff'
+  config.x_xss_protection = { :value => 1, :mode => 'block' }
+  config.x_download_options = 'noopen'
+  config.x_permitted_cross_domain_policies = 'none'
+end
