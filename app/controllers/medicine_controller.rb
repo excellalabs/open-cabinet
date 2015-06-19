3325dd6acefa88b845ecc28512024ee393913333
Jason Witter <jason.witter@excella.com>
2015-06-19 17:47:37 -0400

    Adding cabinet methods.
    Adding cabinet model and relevant tests.

--- app/controllers/medicine_controller.rb
+++ app/controllers/medicine_controller.rb
@@ -1,4 +1,5 @@
 class MedicineController < ApplicationController
+  before_action :find_or_create_cabinet, except: :search
   def index
   end
 
@@ -7,5 +8,27 @@ class MedicineController < ApplicationController
     query = search.query_by_med_name(params[:search_input])
     results = JSON.parse(query.body)['results']
     @display_results = results.map { |med| med['openfda']['brand_name'] }.flatten
+  end
+
+  def cabinet
+  end
+
+  def add_to_cabinet(_params)
+    @cabinet.add_to_cabinet(medicine_params)
+  end
+
+  private
+
+  def medicine_params
+    params.require(:medicine).permit(:name, :set_id)
+  end
+
+  def find_or_create_cabinet
+    if session[:cabinet_id].nil?
+      @cabinet = Cabinet.create!
+      session[:cabinet_id] = @cabinet.id
+    else
+      @cabinet = Cabinet.find_by_id(session[:cabinet_id])
+    end
   end
 end
--- spec/models/cabinet_spec.rb
+++ spec/models/cabinet_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe Cabinet, type: :model do
 
     expect(cabinet.medicines.length).to eq(2)
   end
+
+  it 'can add a new medicine' do
+    cabinet = Cabinet.create!
+
+    cabinet.add_to_cabinet(set_id: '1234', name: 'Advil')
+    expect(cabinet.medicines.length).to eq(1)
+    expect(cabinet.medicines[0].name).to eq('Advil')
+  end
+
+  it 'can add find an existing medicine and add it disregarding name param' do
+    cabinet = Cabinet.create!
+    Medicine.create!(set_id: '1234', name: 'Advil')
+
+    cabinet.add_to_cabinet(set_id: '1234', name: 'Tylenol')
+    expect(cabinet.medicines.length).to eq(1)
+    expect(cabinet.medicines[0].name).to eq('Advil')
+  end
 end
