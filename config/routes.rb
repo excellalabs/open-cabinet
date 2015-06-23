b315e5760f0b44a8cb76b4a9c05da6a0e81dd64a
Aaron Franssell <aaron.franssell@gmail.com>
2015-06-23 15:55:39 -0400

    Delete medicines

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'autocomplete', to: 'medicine#autocomplete'
   get 'cabinet', to: 'medicine#cabinet'
   post 'add_to_cabinet', to: 'medicine#add_to_cabinet'
+  delete 'destroy/:id', to: 'medicine#destroy', as: 'destroy_medicine'
   post 'interactions', to: 'medicine#query_for_all_interactions'
 end
