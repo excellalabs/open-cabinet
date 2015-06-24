de1237f8de1fe17bd1699168705da3bbcef58cdd
Aaron Franssell <aaron.franssell@gmail.com>
2015-06-24 19:54:09 -0400

    Fix rubocop issue

--- spec/helpers/medicine_shelf_helper_spec.rb
+++ spec/helpers/medicine_shelf_helper_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe MedicineShelfHelper do
   describe 'cabinet shows empty shelves' do
     it 'shows empty shelves with 0 medicines' do
       result = helper.show_shelves(Cabinet.new)
-      
+
       expect(result).to have_selector('div.empty-shelf', count: MedicineShelfHelper::MIN_ROWS)
     end
 
     it 'shows minimum number of shelves even if there are 2 medications in the shelf' do
-      result = helper.show_shelves(Cabinet.new(medicines:[Medicine.new(name: 'med1'), Medicine.new(name: 'med2')]))
+      result = helper.show_shelves(Cabinet.new(medicines: [Medicine.new(name: 'med1'), Medicine.new(name: 'med2')]))
 
       expect(result).to have_selector('div.empty-shelf', count: MedicineShelfHelper::MIN_ROWS - 1)
     end
