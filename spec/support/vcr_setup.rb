0a2750236802283abebcab9de05a4b22cebb202e
Aaron Franssell <aaron.franssell@gmail.com>
2015-06-27 10:01:00 -0400

    Swap back to webmock adapter since ignore_hosts directive was not working /w tTTypheous

--- spec/support/vcr_setup.rb
+++ spec/support/vcr_setup.rb
@@ -1,5 +1,5 @@
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/vcr'
-  c.hook_into :typhoeus
-  c.ignore_hosts 'codeclimate.com'
+  c.ignore_host 'codeclimate.com'
+  c.hook_into :webmock
 end
