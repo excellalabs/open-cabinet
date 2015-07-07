ae60519cfc4c3446fdd2cd2c6d082d5f2105d8a2
Aaron Franssell <aaron.franssell@gmail.com>
2015-07-06 20:46:16 -0400

    Remove unused function call

--- app/assets/javascripts/modules/navigation.js
+++ app/assets/javascripts/modules/navigation.js
@@ -98,7 +98,6 @@ Box.Application.addModule('navigation', function(context) {
     init: function() {
       ajax_service = context.getService('ajax-service');
       createOwlNavigation();
-      readMoreReadLessVisibility();
     },
     onclick: function (event, element, elementType) {
       var target_elm = $(event.target);
